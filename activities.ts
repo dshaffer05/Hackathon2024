6abdabb2b245dc9c3aee5a4b9ff9c47daa57e48a
Andrew Shaffer <dshaffer@udel.edu>
2024-03-02 17:06:51 -0500

    Axxiom Activity Added

---
+++ activities.ts
@@ -0,0 +1,10 @@
+import { Activity } from "./dataclasses";
+
+export let Axxiom: Activity = new Activity(
+    "Axxiom Escape Rooms Newark", 
+    "280 E Main St, Newark, DE 19711", 
+    "11AM-8:30PM",
+    "$28",
+    "https://www.axxiomnewark.com",
+    "Multiple escape rooms for groups 4-12 and any age!"
+)
