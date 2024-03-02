4b81db742551329d61f01b1012d0329924ac8f05
Andrew Shaffer <dshaffer@udel.edu>
2024-03-02 17:07:48 -0500

    Dataclass constructor created

--- dataclasses.ts
+++ dataclasses.ts
@@ -1,8 +1,17 @@
-class Activity {
+export class Activity {
     name: string; //Name of the activity
     location: string; //Address of the activity
     time: string; //Time that the activity is open/happening
     price: string; // Price of the activity
     website: string; // activity website
     description: string; // any extra info
+
+    constructor(name, location, time, price, website, description) {
+        this.name = name;
+        this.location = location;
+        this.time = time;
+        this.price = price;
+        this.website = website;
+        this.description = description;
+    }
 }
