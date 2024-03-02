f9d219cf3480404d75006d737688e8db258aaecf
Andrew Shaffer <dshaffer@udel.edu>
2024-03-02 16:43:42 -0500

    Activity Class made

---
+++ dataclasses.ts
@@ -0,0 +1,17 @@
+class Activity {
+    name: string;
+    location: string;
+    schedule: string;
+    price: string;
+    contact: string;
+    other: string;
+}
+class Schedule {
+    sunday: string;
+    monday: string
+    tuesday: string
+    wednesday: string
+    thursday: string
+    friday: string
+    saturday: string
+}
